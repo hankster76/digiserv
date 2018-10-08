9750ff5cc93afac0948b76a0cc9a24a50467bf66
hankster76 <hankster76@users.noreply.github.com>
2018-10-08 09:46:54 -0600

    updated push notification and login screen

--- app/home/home-view-model.ts
+++ app/home/home-view-model.ts
@@ -4,8 +4,9 @@ import { Kinvey } from "kinvey-nativescript-sdk";
 import { ObservableProperty } from "../shared/observable-property-decorator";
 import { Frame, getFrameById, topmost } from "tns-core-modules/ui/frame";
 import { User } from "./user-model";
+import { Push } from "kinvey-nativescript-sdk/push";
 
-export class HomeViewModel extends Observable {
+export class HomeViewModel extends Observable {  
 
     @ObservableProperty() user: User;
     @ObservableProperty() isSignedIn: boolean;
@@ -17,13 +18,38 @@ export class HomeViewModel extends Observable {
         } else {
             this.isSignedIn = false;
         }
+
+        // register for push notifications from Kinvey
+        Push.register({
+            android: {
+            senderID: '390861799348'
+            },
+            ios: {
+            alert: true,
+            badge: true,
+            sound: true
+            }
+        })
+        .then((deviceToken: string) => {
+            //alert("Device registered.  Access token: " + deviceToken);
+            console.log("Device registered for push.  Access token: " + deviceToken);
+        })
+        .catch((error: Error) => {
+            alert("Error: " + error);
+            console.log("Error: " + error);
+        });
+    
+        // this will throw up a push notification in an alert box whenever one is received from Kinvey
+        Push.onNotification((data: any) => {
+            alert(data.body);
+        });
     }
 
     signOut(): void {
+        // log out
         const promise = Kinvey.User.logout()
         .then((data: any) => {
-            //this.user.username = "";
-            //this.user.password = "";
+            console.log("Signed Out");
             this.isSignedIn = false;
         });
     }
@@ -33,9 +59,6 @@ export class HomeViewModel extends Observable {
             .then((result: any) => {
                 console.log("Logged in as: " + result.data.username);
                 this.isSignedIn = true;
-                //const topmostFrame: Frame = topmost();
-                //topmost().navigate("tasks/tasks-page");
-                //topmost().goBack;
             })
             .catch((error) => {
                 console.log("Error: " + JSON.stringify(error));
